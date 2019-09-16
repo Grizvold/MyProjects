bdf1a1bc69c23cfab42e133bcfef36793d92d6ff
ruslan.gorbaty <ruslan105@gmail.com>
2019-09-16 19:09:51 +0300

    modified:   c/WorkSheet8/testPolymorphism.c

--- c/WorkSheet8/testPolymorphism.c
+++ c/WorkSheet8/testPolymorphism.c
@@ -20,7 +20,7 @@ int main()
 
 	AddVal(&elements[0], i_to_add);
 	AddVal(&elements[1], i_to_add);
-	AddVal(&elements[2], i_to_add);
+	/*AddVal(&elements[2], i_to_add);*/
 
 	printf("\n %d \n", *(int *)GetVal(&elements[0]));
 	printf("\n %f \n", *(float *)GetVal(&elements[1]));

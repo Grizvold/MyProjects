edca7ff5447d1d942f057f65a536c3ad6cb4d3e3
ruslan.gorbaty <ruslan105@gmail.com>
2019-09-19 20:07:49 +0300

    new file:   c/WorkSheet9/Itoa_Atoi.c

---
+++ c/WorkSheet9/Itoa_Atoi.c
@@ -0,0 +1,23 @@
+#include <stido.h> /* printf */
+
+static char *Itoa(int i_number, char *str, int base);
+
+static char *Itoa(int i_number, char *str, int base)
+{
+
+
+}
+
+
+int main()
+{
+	
+	int test_int = 123456;
+	char *test_string = NULL;
+	
+	Itoa(test_int, test_string, 10);
+	
+	return 0;
+}
+
+

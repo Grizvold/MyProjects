29401dbb35509b196ce8a510980fd2853a2cb1eb
ruslan.gorbaty <ruslan105@gmail.com>
2019-09-22 15:12:21 +0300

    modified:   ds/src/bitarray.h

--- ds/src/bitarray.h
+++ ds/src/bitarray.h
@@ -1,30 +1,31 @@
 #ifndef __bit_array_h__
 #define __bit_array_h__
 #include <stddef.h> /* size_t */
+
 typedef size_t bit_array_t;
+
+bit_array_t BitArrCreate();
+
+size_t BitArrCountOn(bit_array_t bit_arr);
+size_t BitArrCountOff(bit_array_t bit_arr);
+
+bit_array_t BitArrMirror(bit_array_t bit_arr);
 
 int BitArrIsOn(bit_array_t bit_arr, int index);
 int BitArrIsOff(bit_array_t bit_arr, int index);
 
-void BitArrSetOn(bit_array_t bit_arr, int index);
-void BitArrSetOff(bit_array_t bit_arr, int index);
-
-bit_array_t BitArrRotR(bit_array_t bit_arr, int num_rotations);
-bit_array_t BitArrRotL(bit_array_t bit_arr, int num_rotations);
-bit_array_t BitArrFlip(bit_array_t bit_arr, int index);
-
-bit_array_t BitArrSetAll();
-bit_array_t BitArrResetAll();
-
-int BitArrCountOn(bit_array_t bit_arr);
-int BitArrCountOff(bit_array_t bit_arr);
+bit_array_t BitArrSetAll(bit_array_t bit_arr);
+bit_array_t BitArrResetAll(bit_array_t bit_arr);
 
 char *BitArrToString(bit_array_t bit_arr, char *str);
 
-bit_array_t BitArrMirror(bit_array_t bit_arr);
+bit_array_t BitArrFlip(bit_array_t bit_arr, int index);
 
-bit_array_t BitArrCreate();
+bit_array_t BitArrSetOn(bit_array_t bit_arr, int index);
+bit_array_t BitArrSetOff(bit_array_t bit_arr, int index);
 
+bit_array_t BitArrRotR(bit_array_t bit_arr, size_t num_rotations);
+bit_array_t BitArrRotL(bit_array_t bit_arr, size_t num_rotations);
 
 
 

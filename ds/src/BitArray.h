932a69b1a51f7964204bb4c91da5ba64fca092ee
ruslan.gorbaty <ruslan105@gmail.com>
2019-09-22 13:51:56 +0300

    new file:   ds/src/BitArray.h

---
+++ ds/src/BitArray.h
@@ -0,0 +1,21 @@
+
+
+typedef struct
+{
+	unsigned int arr : size;
+}bit_array_t;
+
+int IsOn(int index);
+int IsOff(int index);
+
+void SetOn(int index);
+void set_off(int index);
+
+void RotR(int num_rotations);
+void RotL(int num_rotations);
+
+void SetAll();
+void ResetAll();
+
+bit_array_t *Create(int size_of_arr);
+

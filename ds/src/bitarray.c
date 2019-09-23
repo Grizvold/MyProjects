7de7708fde561539c37296248e2edaa379aba6f5
ruslan.gorbaty <ruslan105@gmail.com>
2019-09-23 10:12:02 +0300

    modified:   ds/src/bitarray.c 	modified:   ds/src/bitarray.h 	modified:   ds/src/test_bitarray.c

--- ds/src/bitarray.c
+++ ds/src/bitarray.c
@@ -2,11 +2,12 @@
 #include <stddef.h> /* size_t */
 #include <assert.h> /* assert */
 
-static size_t word_size = sizeof(size_t);
+
+static size_t word_size = sizeof(bit_arr_t) * 8;
 static void SwapString(char *str, size_t str_size);
 
 /* count bits that are set to '1' */
-size_t BitArrCountOn(bit_array_t bit_arr)
+size_t BitArrCountOn(bit_arr_t bit_arr)
 {
 	size_t i = 0;
 	size_t counter = 0;
@@ -23,11 +24,11 @@ size_t BitArrCountOn(bit_array_t bit_arr)
 		bit_arr >>= 1;
 	}
 	
-	return (word_size - counter);
+	return counter;
 }
 
 /* count bits that are set to '0' */
-size_t BitArrCountOff(bit_array_t bit_arr)
+size_t BitArrCountOff(bit_arr_t bit_arr)
 {
 	size_t i = 0;
 	size_t counter = 0;
@@ -46,39 +47,43 @@ size_t BitArrCountOff(bit_array_t bit_arr)
 }
 
 /* perform "bit reversal" on our value */
-bit_array_t BitArrMirror(bit_array_t bit_arr)
+bit_arr_t BitArrMirror(bit_arr_t bit_arr)
 {
-	bit_array_t mirror_num = 0;
+	bit_arr_t mask_r = 1;
+	bit_arr_t mask_l = 1;
+	int first = 0, last = 0;
 	size_t i = 0;
+	mask_l <<= (word_size - 1); /* mask_l is pushed left to msb */	
 	
-	for(i = 0; i < word_size/2; i++)
+	for (i = 0; i < word_size / 2 ; i++)
 	{
-		mirror_num <<= 1;
-		mirror_num |= bit_arr & 1;
-		bit_arr >>= 1;
+		first = ((bit_arr & mask_r) == mask_r); /* first will receive arr's lsb value, 1 if the bit is on 0 if off */
+		last = ((bit_arr & mask_l) == mask_l); /* last will receive arr's msb value, 1 if the bit is on 0 if off */
+		
+		bit_arr = BitArrSet(bit_arr, i, last); /* move value of last to first */
+		bit_arr = BitArrSet(bit_arr, (word_size - i - 1), first); /* move value of first to last */
+		
+		mask_r <<= 1; /* forward mask_r to left bit */
+		mask_l >>= 1; /* forward mask_l to right bit */
 	}
-	
-	return mirror_num;
+		
+	return bit_arr;
 }
 
 /* check spesific bit value, if '1' */
-int BitArrIsOn(bit_array_t bit_arr, int index)
+int BitArrIsOn(bit_arr_t bit_arr, int index)
 {
-	int is_on = 0;
-	size_t i = 0;
+	bit_arr >>= (index);
 	
-	bit_arr >>= (index - 1);
-
 	return (bit_arr & 0x01);	
 }
 
 /* check spesific bit value, if '0' */
-int BitArrIsOff(bit_array_t bit_arr, int index)
+int BitArrIsOff(bit_arr_t bit_arr, int index)
 {
-	int is_on = 0, result = 0;
-	size_t i = 0;
+	int result = 0;
 	
-	bit_arr >>= (index - 1);
+	bit_arr >>= (index);
 	bit_arr &= 0x01;
 	
 	if(0 == bit_arr)
@@ -90,19 +95,21 @@ int BitArrIsOff(bit_array_t bit_arr, int index)
 }
 
 /* set all elements in bit_arr to '1' */
-bit_array_t BitArrSetAll(bit_array_t bit_arr)
+bit_arr_t BitArrSetAll(bit_arr_t bit_arr)
 {
+	(void)bit_arr;
 	return ~0;
 }
 
 /* set all elements in bit_arr to '0' */
-bit_array_t BitArrResetAll(bit_array_t bit_arr)
+bit_arr_t BitArrResetAll(bit_arr_t bit_arr)
 {
+	(void)bit_arr;
 	return 0;
 }
 
 /* put elements in bit_arr into string */
-char *BitArrToString(bit_array_t bit_arr, char *str)
+char *BitArrToString(bit_arr_t bit_arr, char *str)
 {
 	size_t i = 0;
 	
@@ -112,6 +119,7 @@ char *BitArrToString(bit_array_t bit_arr, char *str)
 	for(i = 0; i < word_size; i++)
 	{
 		*(str + i) = bit_arr % 2 + '0';
+		bit_arr >>= 1;
 	}
 	
 	SwapString(str, (i));
@@ -134,9 +142,9 @@ static void SwapString(char *str, size_t str_size)
 }
 
 /* flip the value of element in spesific position */
-bit_array_t BitArrFlip(bit_array_t bit_arr, int index)
+bit_arr_t BitArrFlip(bit_arr_t bit_arr, int index)
 {
-	bit_array_t specific_bit = 1;
+	bit_arr_t specific_bit = 1;
 	
 	specific_bit <<= index;
 	bit_arr ^= specific_bit;
@@ -145,9 +153,9 @@ bit_array_t BitArrFlip(bit_array_t bit_arr, int index)
 }
 
 /* set the value of element in spesific position to '1' */
-bit_array_t BitArrSetOn(bit_array_t bit_arr, int index)
+bit_arr_t BitArrSetOn(bit_arr_t bit_arr, int index)
 {
-	bit_array_t specific_bit = 1;
+	bit_arr_t specific_bit = 1;
 	
 	specific_bit <<= index;
 	bit_arr |= specific_bit;
@@ -156,9 +164,9 @@ bit_array_t BitArrSetOn(bit_array_t bit_arr, int index)
 }
 
 /* set the value of element in spesific position to '0' */
-bit_array_t BitArrSetOff(bit_array_t bit_arr, int index)
+bit_arr_t BitArrSetOff(bit_arr_t bit_arr, int index)
 {
-	bit_array_t specific_bit = 1;
+	bit_arr_t specific_bit = 1;
 	
 	specific_bit <<= index;
 	specific_bit = ~specific_bit;
@@ -168,9 +176,9 @@ bit_array_t BitArrSetOff(bit_array_t bit_arr, int index)
 }
 
 /* set the value of element in spesific position to val */
-bit_array_t BitArrSet(bit_array_t bit_arr, int index, int val)
+bit_arr_t BitArrSet(bit_arr_t bit_arr, int index, int val)
 {
-	bit_array_t specific_bit = 1;
+	bit_arr_t specific_bit = 1;
 	
 	specific_bit <<= index;
 	/* check if val is 0 or 1 */
@@ -189,9 +197,9 @@ bit_array_t BitArrSet(bit_array_t bit_arr, int index, int val)
 }
 
 
-bit_array_t BitArrRotR(bit_array_t bit_arr, size_t num_rotations)
+bit_arr_t BitArrRotR(bit_arr_t bit_arr, size_t num_rotations)
 {
-	bit_array_t temp = 0;
+	bit_arr_t temp = 0;
 	
 	num_rotations %= word_size;
 	temp = (bit_arr << (word_size - num_rotations));
@@ -200,9 +208,9 @@ bit_array_t BitArrRotR(bit_array_t bit_arr, size_t num_rotations)
 	return (bit_arr | temp);
 }
 
-bit_array_t BitArrRotL(bit_array_t bit_arr, size_t num_rotations)
+bit_arr_t BitArrRotL(bit_arr_t bit_arr, size_t num_rotations)
 {
-	bit_array_t temp = 0;
+	bit_arr_t temp = 0;
 	
 	num_rotations %= word_size;
 	temp = (bit_arr >> (word_size - num_rotations));
--- ds/src/test_bitarray.c
+++ ds/src/test_bitarray.c
@@ -1,21 +1,67 @@
 #include "bitarray.h" /* bit array func */
 #include <stdio.h> /* prtinf */
+#include <assert.h> /* assert */
+#include <stdlib.h> /* malloc */
 
 int main()
 {
-	bit_array_t test_arr;	
-
-/*	test_arr = BitArrSetAll(test_arr);	*/
-/*	printf("\n%lu\n", test_arr);	*/
-
-/*	test_arr = BitArrResetAll(test_arr);	*/
-/*	printf("\n%lu\n", test_arr);	*/
+	bit_arr_t test_arr;	
+	char *str = (char *)malloc(65);
 	
-/*	test_arr = BitArrSetOn(test_arr, 15);	*/
-/*	printf("\n%lu\n", test_arr);	*/
-	
-	
+	assert(NULL != str);
 
+/*	test_arr = BitArrSetAll(test_arr);	
+	printf("\n%lu\n", test_arr);	*/
+
+/*	test_arr = BitArrResetAll(test_arr);	
+	printf("\n%lu\n", test_arr);	*/
+	
+/*	test_arr = BitArrSetOn(test_arr, 15);	
+	printf("\n%lu\n", test_arr);	*/
+	
+/*	test_arr = BitArrSetOff(test_arr, 15);
+	printf("\n%lu\n", test_arr);	*/
+	
+/*	test_arr = BitArrSetOn(test_arr, 15);
+	printf("\n%lu\n", test_arr);
+	test_arr = BitArrFlip(test_arr, 15);
+	printf("\n%lu\n", test_arr);	*/
+	
+/*	test_arr = BitArrSet(test_arr, 15, 1);
+	printf("\n%lu\n", test_arr);
+	test_arr = BitArrSet(test_arr, 15, 0);
+	printf("\n%lu\n", test_arr);	*/
+	
+/*	test_arr = BitArrSet(test_arr, 15, 1);
+	printf("\n%lu\n", test_arr);
+	printf("\nIs on: %d\n", BitArrIsOn(test_arr, 15));
+	
+	test_arr = BitArrSet(test_arr, 15, 0);
+	printf("\n%lu\n", test_arr);	
+	printf("\nIs off: %d\n", BitArrIsOff(test_arr, 15));	*/
+	
+/*	test_arr = 57552;
+	test_arr = BitArrMirror(test_arr);
+	printf("\n%lu\n", test_arr);	*/
+	
+/*	test_arr = 892453;
+	str = BitArrToString(test_arr, str);
+	printf("\n%lu\n", test_arr);
+	printf("\nstring is %s\n", str);	*/
+	
+/*	test_arr = 892453;
+	printf("\n%lu\n", BitArrCountOn(test_arr));
+	printf("\n%lu\n", BitArrCountOff(test_arr));	*/
+	
+	test_arr = 892453;
+	test_arr = BitArrRotR(test_arr, 5);
+	test_arr = BitArrRotL(test_arr, 5);
+	printf("\n%lu\n", test_arr);
+	
+	
+	
+	
+	
 	return 0;
 }
 

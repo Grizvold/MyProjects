b77d4cdc48ec6e3c6594818135ccc1a4a27cfc62
ruslan.gorbaty <ruslan105@gmail.com>
2019-09-25 17:17:33 +0300

    dynamic array 	new file:   ds/src/vector.c 	new file:   ds/src/vector.h

---
+++ ds/src/vector.c
@@ -0,0 +1,8 @@
+
+struct vector
+{
+	size_t element_size;
+	size_t capacity;
+	size_t size;
+	void *buffer;
+};
---
+++ ds/src/vector.h
@@ -0,0 +1,24 @@
+#ifndef __vector_h__
+#define __vector_h__
+
+#include <stddef.h> /* size_t */
+
+typedef struct vector vector_t;
+
+VectorCreate(size_t element_size, size_t num_of_elements);
+
+VectorDestroy(vector_t *element);
+
+void *VectorGetItemAddress(vector_t *element);
+
+void VectorPushBack(vector_t *element, void *element);
+
+void VectorPopBack(vector_t *element);
+
+size_t VectorSize(const vector_t *element);
+
+size_t VectorCapacity(const vector_t *element);
+
+size_t VectorReserve(vector_t *element, size_t num_elements);
+
+#endif /* vector */

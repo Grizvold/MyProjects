5e42a9f20ffaa36159a5f817198444209d7c95f6
ruslan.gorbaty <ruslan105@gmail.com>
2019-10-10 14:56:25 +0300

    new api of dll 	renamed:    ds/src/cir_buffer.c -> ds/src/cir_buffer/cir_buffer.c 	renamed:    ds/src/cir_buffer.h -> ds/src/cir_buffer/cir_buffer.h 	renamed:    ds/src/cir_buffer_test.c -> ds/src/cir_buffer/cir_buffer_test.c 	new file:   ds/src/dll.h 	new file:   ds/src/dll/dll.c 	new file:   ds/src/dll/dll.h 	new file:   ds/src/dll/dll_test.c

---
+++ ds/src/dll.h
@@ -0,0 +1,105 @@
+#ifndef __DLL_H__
+#define __DLL_H__
+
+#include <stddef.h> /* size_t */
+
+/*########################### typedefs ###########################*/
+
+/* typedef of node */
+typedef struct dll_node dll_node_t;
+
+/* typedef of list */
+typedef struct dll_list dll_t;
+
+
+/* typedef of iterator to dll */
+typedef struct dll_iter
+{
+	dll_note_t *current;
+	
+}dll_iter_t;
+
+/* 	typedef of action function for ForEach function	*/
+typedef int (*dll_act_func_t)(void *data, void *param);
+
+/* 	typedef of action function for Find function		*/
+typedef int (*dll_cmp_func_t)(void *data, void *param);
+/*################################################################*/
+
+/*####################### DLL Funtionality #######################*/
+
+/* *Create new doubly linked list
+   *Return pointer to new created list	*/
+dll_t *DLLCreate();
+
+/* *Free desired doubly linked list	*/
+void DLLDestroy(dll_t *target_dll);
+
+/* *Insert new element in dll
+   *Return  iterator to next item in case of success
+    Return EndIterator in case of failure			
+   *Complexity O(1)								*/
+dll_iter_t DLLInsert(dll_iter_t dll_iterator, const void *data);
+
+/* *Remove element from dll
+   *Return  iterator to next item in case of success
+    Return EndIterator in case of failure			
+   *Complexity O(1)								*/    
+dll_iter_t DLLRemove(dll_iter_t dll_iterator);
+
+/* *Push new element at the start of dll
+   *Return iterator to the new element
+    In case of failure return EndIterator			
+   *Complexity O(1)								*/
+dll_iter_t DLLPushFront(dll_iter_t dll_iterator, const void *data);
+
+/* *Pop first element of the dll
+   *Returns data of the first element				
+   *Complexity O(1)								*/
+void *DLLPopFront(dll_iter_t dll_iterator);
+
+/* *Push new element at the end of dll
+   *Return iterator to the new element
+    In case of failure return EndIterator	
+   *Complexity O(1)								*/
+dll_iter_t DLLPushBack(dll_iter_t dll_iterator, const void *data);
+
+/* *Pop last element of the dll
+   *Returns data of the last element
+   *Complexity O(1)								*/
+void *DLLPopBack(dll_iter_t dll_iterator);
+
+/* *Find element in targeted dll
+   *Return iterator to element in case of success
+    Return EndIterator in case of failure		
+   *Complexity O(n)	*/
+dll_iter_t DLLFind(dll_iter_t start, dll_iter_t end, dll_cmp_func_t cmp_func, void *param);
+
+/* *Perform action on each element of the dll
+   *Return status of performed action
+    0 - Success
+    1 - Failure	
+   *Complexity O(n)								*/
+int DLLForEach(dll_iter_t start, dll_iter_t end, dll_act_func_t act_func, void *param);
+
+/* *Insert source dll size from-to into dest dll
+   *Return iterator "where"
+   *In case of failure return iterator of EndIterator	*/
+dll_iter_t DLLSplice(dll_iter_t where, dll_iter_t from, dll_iter_t to);
+
+/* *Return number of elements in target dll			
+   *Complexity O(n)								*/
+size_t DLLSize(const dll_t *target_list);
+/*################################################################*/
+
+/*####################### DLL Iterator Functionality #######################*/
+/* *Receives targeted dll list
+   *Returns first element of dll					*/
+dll_iter_t DLLBegin(const dll_t *list);
+
+/* *Receives targeted dll list
+   *Returns iterator refering to <past-the-end> element in vector container */
+dll_iter_t DLLEnd(const dll_t *list);
+/*##########################################################################*/
+
+#endif __DLL_H__
---
+++ ds/src/dll/dll.h
@@ -0,0 +1,15 @@
+#ifndef __DLL_H__
+#define __DLL_H__
+
+#include <stddef.h> /* size_t */
+
+typedef struct dll dll_t;
+
+typedef struct dll_node dll_node_t;
+
+typedef struct
+{
+	
+}
+
+#endif __DLL_H__

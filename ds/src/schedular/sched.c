21bb344d29c51fed8e4e017abf3e49c3cef26951
ruslan.gorbaty <ruslan105@gmail.com>
2019-10-22 21:03:07 +0300

    modified:   ds/src/schedular/sched.c 	modified:   ds/src/schedular/sched.h

--- ds/src/schedular/sched.c
+++ ds/src/schedular/sched.c
@@ -1,4 +1,77 @@
-#include <stddef.h>
+#include <stddef.h> /* size_t */
+#include <stdio.h>  /* perror */
 
-#include "schedular.h"
 
+#include "sched.h"
+#include "pq.h"
+#include "uid.h"
+
+/*########################### typedefs ################################*/
+
+struct sched
+{
+	pq_t *p_queue;
+};
+
+typedef struct task
+{
+	ilrd_uid_t task_id;
+	size_t interval_sec;
+	time_t next_time;
+	opration_func_t action_func;
+	void *action_func_param;
+}task_t;
+
+/*#####################################################################*/
+
+/*######################## Auxiliary Func #############################*/
+
+int IsBefore(void *data_1, void *data_2, void *param);
+
+/*#####################################################################*/
+
+/*####################### Schedule Funtionality #######################*/
+
+sched_t *SchedCreate()
+{
+	sched_t *new_sched = NULL;
+	
+	new_sched = (sched_t *)malloc(sizeof(*new_sched));
+	if(NULL == new_sched)
+	{
+		perror("SchedCreate malloc failed");
+		
+		return NULL;
+	}
+	
+	new_sched->p_queue = PQCreate(&IsBefore, NULL);
+	if(NULL == new_sched->p_queue)
+	{
+		perror("SchedCreate PQCreate malloc failed");
+		
+		free(new_sched);
+		
+		return NULL;
+	}
+	
+	return new_sched;
+}
+
+uid_t SchedAddTask(sched_t *sched,
+ 				size_t interval_sec, 
+ 				operation_func_t act_func, 
+ 				void *param)
+{
+	
+}
+
+/*#####################################################################*/
+
+/*######################## Auxiliary Func #############################*/
+
+int IsBefore(void *data_1, void *data_2, void *param)
+{
+	
+}
+
+/*#####################################################################*/
--- ds/src/schedular/sched.h
+++ ds/src/schedular/sched.h
@@ -5,20 +5,30 @@
 
 #include "uid.h"
 
+/*########################### typedefs ################################*/
+
 typedef struct sched sched_t;
 
 typedef int(*operation_func_t)(void *param);
 
+/*#####################################################################*/
+
+/*####################### Schedule Funtionality #######################*/
 sched_t *SchedCreate();
 
-uid_t SchedAddTast(sched_t *sched, size_t interval_sec, operation_func_t act_func, void *param);
+uid_t SchedAddTask(sched_t *sched,
+ 				size_t interval_sec, 
+ 				operation_func_t act_func, 
+ 				void *param);
 
-void SchedRemove(sched_t *sched, uid_t task);
+void SchedRemoveTask(sched_t *sched, ilrd_uid_t uid);
 
 void SchedRun(sched_t *sched);
 
 void SchedStop(sched_t *sched);
 
 void SchedDestroy(sched_t *sched);
+
+/*#####################################################################*/
 
 #endif /* __SCHED_H__ */

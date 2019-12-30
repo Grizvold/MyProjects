063fdad6286ffa6ac038e0a20e789e75b8eb58f2
ruslan.gorbaty <ruslan105@gmail.com>
2019-12-30 18:30:56 +0200

    update

--- java/eclipse/Complex/src/il/co/ilrd/complex/ComplexTest.java
+++ java/eclipse/Complex/src/il/co/ilrd/complex/ComplexTest.java
@@ -14,10 +14,16 @@ public class ComplexTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Complex testComplex = new Complex(2, 3);
-		System.out.println(testComplex);
-		Complex resultComplex =  testComplex.add(5);
-		System.out.println(resultComplex);
+		Complex complexNum1 = new Complex(1,2);
+		Complex complexNum2 = new Complex(2,3);
+		Complex complexNum3 = new Complex(3,4);
+		Complex complexNum4 = new Complex(4,5);
+		
+		complexNum3 = complexNum1.add(complexNum2).substract(complexNum4);
+		System.out.println(complexNum3);
+		
+		complexNum3.setImaginary(5);
+		System.out.println(complexNum3);
 	}
 
 }

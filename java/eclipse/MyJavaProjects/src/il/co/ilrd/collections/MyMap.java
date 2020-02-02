f18efdb895069500547d094d54ff500290ee6a8c
ruslan.gorbaty <ruslan105@gmail.com>
2020-02-02 20:09:38 +0200

    update

--- java/eclipse/MyJavaProjects/src/il/co/ilrd/collections/MyMap.java
+++ java/eclipse/MyJavaProjects/src/il/co/ilrd/collections/MyMap.java
@@ -13,110 +13,126 @@ import java.util.Set;
 public class MyMap<K, V> implements Map<K, V>{
 	
 	private ArrayList<LinkedList<Pair<K, V>>> hashMap;
-	private static final int size = 16;
+	private static final int numOfBuckets = 16;
 	
 	public MyMap() {
-		hashMap = new ArrayList<LinkedList<Pair<K,V>>>(size);
+		hashMap = new ArrayList<LinkedList<Pair<K,V>>>(numOfBuckets);
+		
+		for (int i = 0; i < numOfBuckets; ++i) {
+			hashMap.add(new LinkedList<Pair<K,V>>());
+		}
 	}
 	
-	private class EntrySet extends AbstractSet<Pair<K, V>> {
+	private class EntrySet extends AbstractSet<Entry<K, V>> {
 
-		private class EntryIterator implements Iterator<Pair<K, V>> {
-
+		private class EntryIterator implements Iterator<Entry<K, V>> {
+			int currentListIndex = 0;
+			Iterator<Pair<K, V>> listIterator = hashMap.get(currentListIndex).iterator();
+			
 			@Override
 			public boolean hasNext() {
-				// TODO Auto-generated method stub
+				if(listIterator.hasNext()) {
+					return true;
+				}
+				
+				for (int i = currentListIndex + 1; i < numOfBuckets; ++i) {
+					if(!hashMap.get(i).isEmpty()) {
+						return true;
+					}
+				}
+				
 				return false;
 			}
 
 			@Override
-			public Pair<K, V> next() {
-				// TODO Auto-generated method stub
-				return null;
+			public Entry<K, V> next() {
+				if(!listIterator.hasNext()) {
+					
+					for (++currentListIndex; currentListIndex < numOfBuckets; ++currentListIndex) {
+						if(!hashMap.get(currentListIndex).isEmpty()) {
+							listIterator = hashMap.get(currentListIndex).iterator();
+							break;
+						}
+					}
+				}
+				return listIterator.next();
 			}
 		}
 		
 		@Override
-		public Iterator<Pair<K, V>> iterator() {
-			// TODO Auto-generated method stub
-			return null;
+		public Iterator<Entry<K, V>> iterator() {
+			return new EntryIterator();
 		}
 
 		@Override
 		public int size() {
-			// TODO Auto-generated method stub
-			return 0;
+			return MyMap.this.size();
 		}
 	}
 	
 	private class Values extends AbstractCollection<V> {
 
 		private EntrySet entry = new EntrySet();
+		private EntrySet.EntryIterator valIterator = entry.new EntryIterator();
 		
 		private class ValuesIterator implements Iterator<V> {
 
 			@Override
 			public boolean hasNext() {
-				// TODO Auto-generated method stub
-				return false;
+				return valIterator.hasNext();
 			}
 
 			@Override
 			public V next() {
-				// TODO Auto-generated method stub
-				return null;
+				return valIterator.next().getValue();
 			}
 		}
 		
 		@Override
 		public Iterator<V> iterator() {
-			// TODO Auto-generated method stub
-			return null;
+			return new ValuesIterator();
 		}
 
 		@Override
 		public int size() {
-			// TODO Auto-generated method stub
-			return 0;
+			return MyMap.this.size();
 		}
 	}
 	
 	private class KeysSet extends AbstractSet<K> {
 
 		private EntrySet entry = new EntrySet();
+		private EntrySet.EntryIterator keyIterator = entry.new EntryIterator();
 		
 		private class KeysIterator implements Iterator<K> {
 
 			@Override
 			public boolean hasNext() {
-				// TODO Auto-generated method stub
-				return false;
+				return keyIterator.hasNext();
 			}
 
 			@Override
 			public K next() {
-				// TODO Auto-generated method stub
-				return null;
+				return keyIterator.next().getKey();
 			}
 		}
 		
 		@Override
 		public Iterator<K> iterator() {
-			// TODO Auto-generated method stub
-			return null;
+			return new KeysIterator();
 		}
 
 		@Override
 		public int size() {
-			// TODO Auto-generated method stub
-			return 0;
+			return MyMap.this.size();
 		}
 	}
 	
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		for (LinkedList<Pair<K, V>> linkedList : hashMap) {
+			linkedList.clear();
+		}
 	}
 
 	@Override
@@ -139,8 +155,12 @@ public class MyMap<K, V> implements Map<K, V>{
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		for (LinkedList<Pair<K, V>> linkedList : hashMap) {
+			if(!linkedList.isEmpty()) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
@@ -151,8 +171,20 @@ public class MyMap<K, V> implements Map<K, V>{
 
 	@Override
 	public V put(K key, V value) {
-		// TODO Auto-generated method stub
-		return null;
+		V oldValue = null;
+		LinkedList<Pair<K, V>> list = hashMap.get(getIndex(key));
+		Pair<K, V> newPair = Pair.of(key, value);
+		
+		for (Pair<K, V> pair : list) {
+			if(pair.getKey().equals(pair.getKey())) {
+				oldValue = pair.getValue();
+				list.remove(pair);
+				break;
+			}
+		}
+		
+		list.add(newPair);		
+		return oldValue;
 	}
 
 	@Override
@@ -169,19 +201,25 @@ public class MyMap<K, V> implements Map<K, V>{
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		int size = 0;
+		for (LinkedList<Pair<K, V>> linkedList : hashMap) {
+			size += linkedList.size();
+		}
+		return size;
 	}
 
 	@Override
 	public Set<Entry<K, V>> entrySet() {
-		// TODO Auto-generated method stub
-		return null;
+		return new EntrySet();
 	}
 	
 	@Override
 	public Collection<V> values() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private int getIndex(Object key) {
+		return key.hashCode() % numOfBuckets;
 	}
 }
---
+++ java/eclipse/MyJavaProjects/src/il/co/ilrd/my_tests/MyMapTestJUnit.java
@@ -0,0 +1,29 @@
+package il.co.ilrd.my_tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import il.co.ilrd.collections.MyMap;
+
+class MyMapTestJUnit {
+	
+	MyMap<Integer, String> testMap;
+	
+	@BeforeEach
+	void BeforeEach() {
+		testMap = new MyMap<Integer, String>();
+	}
+
+	@Test
+	void testSize() {
+		assertEquals(0, testMap.size());
+	}
+	
+	@Test
+	void testIsEmpty() {
+		assertTrue(testMap.isEmpty());
+	}
+
+}

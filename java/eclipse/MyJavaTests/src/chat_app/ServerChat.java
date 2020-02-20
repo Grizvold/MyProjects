484906ff101fb99d97d14cb665115764c290edd7
ruslan.gorbaty <ruslan105@gmail.com>
2020-02-20 18:34:16 +0200

    update

---
+++ java/eclipse/MyJavaTests/src/chat_app/ServerChat.java
@@ -0,0 +1,65 @@
+package chat_app;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JTextField;
+
+public class ServerChat extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField sendingMessage;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ServerChat frame = new ServerChat();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ServerChat() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 707, 630);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JTextArea chatArea = new JTextArea();
+		contentPane.add(chatArea, BorderLayout.CENTER);
+		
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.setBounds(50, 50, 100, 100);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		contentPane.add(btnNewButton, BorderLayout.WEST);
+		
+		sendingMessage = new JTextField();
+		contentPane.add(sendingMessage, BorderLayout.SOUTH);
+		sendingMessage.setColumns(10);
+	}
+
+}

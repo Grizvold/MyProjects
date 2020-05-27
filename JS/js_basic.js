caa8a32d493b470331464bce3987aa7d72c32761
GIT_NAME <GIT_EMAIL>
2020-05-27 18:27:43 +0300

    update

--- JS/js_basic.js
+++ JS/js_basic.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 /*var primitive = "september";
 primitive.vowels = 3;
 console.log(primitive.vowels);
@@ -10,10 +10,62 @@ console.log(bar);
 bar = bar.toUpperCase();
 console.log(bar);*/
 
-var string = "Some funny string";
-string[0] = "s";
-console.log(string);
+// var string = "Some funny string";
+// string[0] = "s";
+// console.log(string);
 
-const actors = ["Morgan Freeman", "Jennifer Anniston"];;
-actors[2] = "Tom Cruise";
+// const actors = ["Morgan Freeman", "Jennifer Anniston"];;
+// actors[2] = "Tom Cruise";
 //actors = [];
+
+// Number.prototype.getType = function(){
+//     return typeof this;
+// }
+
+// (12345).getType;
+// typeof 12345;
+
+// if(new Boolean(false)){
+//     console.log("woot?");
+// }
+
+// var x = new Boolean(false);
+// //x = null;
+// console.log(x);
+// if(x.Boolean){
+//     console.log("woot?");
+// }
+
+// var x = "hello";
+// if(Boolean(x) == true){
+//     console.log("inside if");
+// }
+
+// var x = [1, 2, 3];
+// var y = x;
+// y === x;
+// console.log(y);
+// console.log(x);
+// if (y === [1, 2, 3]) {
+//   console.log("y === [1, 2, 3] true");  
+// } 
+
+// if (x === [1, 2, 3]) {
+//     console.log("x === [1, 2, 3] true");  
+// } 
+
+// function double(x){
+//     return x * 2;
+// }
+// console.log(double(4, true, "hedgehog"));
+
+// var funcs = [];
+// for (var i = 0; i < 10; i++) {
+//     funcs.push(funcs()){
+//         console.log(i);
+//     }
+// }
+
+// funcs.forEach(function(funcs) {
+//     funcs();
+// });

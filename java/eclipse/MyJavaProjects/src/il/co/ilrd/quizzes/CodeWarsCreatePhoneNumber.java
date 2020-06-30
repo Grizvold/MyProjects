0e7b6d2c60d49d92cced68e0c64e8ba1f22b2068
GIT_NAME <GIT_EMAIL>
2020-06-30 17:50:44 +0300

    update

--- java/eclipse/MyJavaProjects/src/il/co/ilrd/quizzes/CodeWarsCreatePhoneNumber.java
+++ java/eclipse/MyJavaProjects/src/il/co/ilrd/quizzes/CodeWarsCreatePhoneNumber.java
@@ -3,15 +3,34 @@ package il.co.ilrd.quizzes;
 public class CodeWarsCreatePhoneNumber {
 
 	public static void main(String[] args) {
+		System.out.println(createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
 		
+		System.out.println(short_createPhoneNumer(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
 	}
 	
 	public static String createPhoneNumber(int[] numbers) {
-//		return String.format("(" + %d%d%d + ")", 
-//							numbers[0],
-//							);// Your code here!
-		
-		return null;
-	 }
+		String area = "";
+		String second_three = "";
+		String the_rest = "";
+		for (int i = 0; i < numbers.length; ++i) {
+			if(i < 3) {
+				area += numbers[i];
+			}
+			else if (i > 2 && i < 6) {
+				second_three += numbers[i];
+			}
+			else {
+				the_rest += numbers[i];
+			}
+		}
+		return "(" + area + ")" + " " + second_three + "-" + the_rest;
+	}
+	
+	public static String short_createPhoneNumer(int[] numbers) {
+		return String.format("(%d%d%d) %d%d%d-%d%d%d%d", 
+				numbers[0],numbers[1],numbers[2],
+				numbers[3],numbers[4],numbers[5],
+				numbers[6],numbers[7],numbers[8],numbers[9]);
+	}
 
 }
---
+++ java/eclipse/SpringQuickStart/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/springGet")
+	public String helloTest(@RequestParam(value = "name", defaultValue = "world") String name) {
+		return String.format("Hello %s!", name);
+	}
+	
+	@PostMapping("/springPost")
+	public String postTest() {
+		return null;
+	}
+}

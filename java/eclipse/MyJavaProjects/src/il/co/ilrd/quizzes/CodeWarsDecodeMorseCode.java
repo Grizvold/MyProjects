49275784a67b0ab96c1ba9ac4d6157e4c5db0422
GIT_NAME <GIT_EMAIL>
2020-07-27 10:07:38 +0300

    update

---
+++ java/eclipse/MyJavaProjects/src/il/co/ilrd/quizzes/CodeWarsDecodeMorseCode.java
@@ -0,0 +1,20 @@
+package il.co.ilrd.quizzes;
+
+public class CodeWarsDecodeMorseCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static String decode(String morseCode) {
+	      String result = "";
+	      for(String word : morseCode.trim().split("   ")) {
+	        for(String letter : word.split("\\s+")) {
+	          result += MorseCode.get(letter);
+	        }
+	        result += ' ';
+	      }
+	      return result.trim();
+	}
+}
